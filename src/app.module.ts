dc43d504cbfc48cb85c2cf8afdf7bfacae1d914b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-07-17 13:38:54 -0400

    conexion with the database mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Guarenas852456+',
+      database: 'nestdb',
+      entities: [__dirname + '/**/*.entity{.ts, .js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
