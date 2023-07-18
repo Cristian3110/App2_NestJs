b5e85421def7fc75470d7ff802ff7fb5b5f7ff48
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-07-18 00:29:11 -0400

    configuration of petition http GET from the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user-dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
   @Post()
-  createUser(@Body() newUser: CreateUserDto) {
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
     return this.userService.createUser(newUser);
+  }
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
   }
 }
